9d7a3489ec30440e905eeb149c807c322b9f7fbb
Jones-bass <jonesbass.tb@gmail.com>
2022-06-20 22:57:56 -0300

    Starting Project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Home from './src/screem/Home/Index';
+
+export default function App() {
+  return (
+    <>
+      <Home/>
+      <StatusBar style="auto" />
+    </>
+  );
+}
+
+
---
+++ src/screem/Home/Index.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import {
+  Container,
+  Title,
+} from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Title>Começando uma nova jornada de aprendizado!</Title>
+    </Container>
+  );
+}
+
+
