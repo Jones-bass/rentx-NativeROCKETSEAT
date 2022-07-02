35e4f1ffa9b65114beac128e6dc177b99678bd52
Jones-bass <jonesbass.tb@gmail.com>
2022-07-02 11:41:20 -0300

    Calendars

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,58 +1,79 @@
-import React from 'react';
-import { 
-    Calendar as CustomCalendar,
-    LocaleConfig,
-} from 'react-native-calendars';
+import React from "react";
+import {
+  Calendar as CustomCalendar,
+  LocaleConfig,
+  DateCallbackHandler,
+} from "react-native-calendars";
 
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 
-import { useTheme } from 'styled-components';
+import { useTheme } from "styled-components";
+import { ptBR } from "./localeConfig";
+import { generateInterval } from "./generateInterval";
 
-LocaleConfig.locales['pt-br'] = {
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 
-    'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Junh', 'Julh', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+LocaleConfig.locales["pt-br"] = ptBR;
+LocaleConfig.defaultLocale = "pt-br";
+
+interface MarkedDateProps {
+  [date: string]: {
+    color: string;
+    textColor: string;
+    disabled?: boolean;
+    disableTouchEvent?: boolean;
+  };
 }
-LocaleConfig.defaultLocale = 'pt-br';
 
+interface DayProps {
+  dateString: string;
+  day: number;
+  month: number;
+  year: number;
+  timestamp: number;
+}
 
+interface CalendarProps {
+  markedDates: MarkedDateProps;
+  onDayPress: DateCallbackHandler;
+}
 
-export function Calendar () {
-    const theme = useTheme();
-    return (
-    <CustomCalendar    
-        renderArrow={(direction) => 
-            <Feather 
-                size={24} 
-                color={theme.colors.shape} 
-                name={direction === 'left' ? 'chevron-left' : 'chevron-right'} 
-            /> 
-        }
-        headerStyle={{
-            backgroundColor: theme.colors.background_secondary,
-            borderBottomWidth: 0.5,
-            borderBottomColor: theme.colors.text_detail,
-            paddingBottom: 10,
-            marginBottom: 10
-        }}
-        theme={{
-            textDayHeaderFontFamily: theme.fonts.primary_500,
-            textDayHeaderFontSize: 10,
-            textDayFontFamily: theme.fonts.primary_400,
-            
-            textMonthFontSize: 20,
-            textMonthFontFamily: theme.fonts.secondary_600,
-            monthTextColor: theme.colors.title,
-            
-            arrowStyle: {
-                marginHorizontal: -15
-            }
-        }}
+function Calendar({ markedDates, onDayPress }: CalendarProps) {
+  const theme = useTheme();
+  return (
+    <CustomCalendar
+      renderArrow={(direction) => (
+        <Feather
+          size={24}
+          color={theme.colors.shape}
+          name={direction === "left" ? "chevron-left" : "chevron-right"}
+        />
+      )}
+      headerStyle={{
+        backgroundColor: theme.colors.background_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_detail,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+      theme={{
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textDayHeaderFontSize: 10,
+        textDayFontFamily: theme.fonts.primary_400,
 
-        firstDay={1}  
+        textMonthFontSize: 20,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        monthTextColor: theme.colors.title,
+
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      firstDay={1}
+      minDate={new Date()}
+      markingType="period"
+      markedDates={markedDates}
+      onDayPress={onDayPress}
     />
-        
-    );
-};
+  );
+}
+
+export { Calendar, MarkedDateProps, DayProps, generateInterval };
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,81 +1,131 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
-import { useTheme } from 'styled-components';
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert, StatusBar } from "react-native";
+import { useTheme } from "styled-components";
 
-import ArrowSvg from '../../assets/arrow.svg';
-import { BackButton } from '../../components/BackButton';
-import { Button } from '../../components/Button';
-import { Calendar } from '../../components/Calendar';
+import ArrowSvg from "../../assets/arrow.svg";
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import { getPlatformDate } from "../../utils/getPlatformDate";
+import {
+  Calendar,
+  DayProps,
+  generateInterval,
+  MarkedDateProps,
+} from "../../components/Calendar";
 
-import { 
-    Container,
-    Header,
-    Title,
-    RentalPeriod,
-    DateInfo,
-    DateTitle,
-    Footer,
-    Content,
-    DateValue,
-} from './styles';
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  Footer,
+  Content,
+  DateValue,
+} from "./styles";
+import { format } from "date-fns";
 
 interface RentalPeriod {
-    startFormatted: string;
-    endFormatted: string;
-};
+  startFormatted: string;
+  start: number;
+  endFormatted: string;
+  end: number;
+}
 
-export function Scheduling () {
-    const theme = useTheme();
-    const navigation = useNavigation();
+export function Scheduling() {
+  const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>(
+    {} as DayProps
+  ); //ultima da ta selecionada
+  const [markedDates, setMarkedDates] = useState<MarkedDateProps>(
+    {} as MarkedDateProps
+  );
+  const [rentalPeriod, setRentalPeriod] = useState<RentalPeriod>(
+    {} as RentalPeriod
+  );
 
-    function handleConfirmRental() {
-      navigation.navigate('SchedulingDetails')    
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  function handleConfirmRental() {
+    if (!rentalPeriod.start || !rentalPeriod.end) {
+      Alert.alert('Selecione o intervalo para alugar.');
+    } else {
+      navigation.navigate("SchedulingDetails");
     }
-    
-    return (
+  }
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleChangeDate(date: DayProps) {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+
+    if (start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+
+    setLastSelectedDate(end);
+    const interval = generateInterval(start, end);
+    setMarkedDates(interval);
+
+    const firstDate = Object.keys(interval)[0];
+    const endDate = Object.keys(interval)[Object.keys(interval).length - 1];
+
+    setRentalPeriod({
+      startFormatted: format(
+        getPlatformDate(new Date(firstDate)),
+        "dd/MM/yyyy"
+      ),
+      endFormatted: format(getPlatformDate(new Date(endDate)), "dd/MM/yyyy"),
+    });
+  }
+
+  return (
     <Container>
-        <StatusBar 
-            barStyle='light-content' 
-            backgroundColor="transparent" 
-            translucent
-        />        
-        <Header> 
-            <BackButton 
-                onPress={() => {}}
-                color={theme.colors.shape}
-            />  
-            <Title>
-                Escolha uma{'\n'}
-                data de início e{'\n'}
-                fim do aluguel{'\n'}
-            </Title>
-            
-            <RentalPeriod>
-                <DateInfo>
-                    <DateTitle>DE</DateTitle>
-                    <DateValue selected={false}>27/05/2022</DateValue>
-                </DateInfo>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+        <Title>
+          Escolha uma{"\n"}
+          data de início e{"\n"}
+          fim do aluguel{"\n"}
+        </Title>
 
-                <ArrowSvg />
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={!!rentalPeriod.startFormatted}>
+              {rentalPeriod.startFormatted}
+            </DateValue>
+          </DateInfo>
 
-                <DateInfo>
-                    <DateTitle>ATÉ</DateTitle>
-                    <DateValue selected={false}>27/06/2022</DateValue>
-                </DateInfo>                
-            </RentalPeriod>
+          <ArrowSvg />
 
-        </Header>
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={!!rentalPeriod.endFormatted}>
+              {rentalPeriod.endFormatted}
+            </DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
 
-        <Content>
-            <Calendar/>
-        </Content>
+      <Content>
+        <Calendar markedDates={markedDates} onDayPress={handleChangeDate} />
+      </Content>
 
-        <Footer>
-            <Button 
-                title="Confirmar" onPress={handleConfirmRental}/>
-        </Footer>
-        
+      <Footer>
+        <Button title="Confirmar" onPress={handleConfirmRental} />
+      </Footer>
     </Container>
-    );
+  );
 }
