f7839757ecce9ee3ce77a21d3503d2fef94a198f
jones-bass <jonesbass.tb@gmail.com>
2024-01-08 20:17:08 -0300

    estruturando server

---
+++ server/src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { CategoriesRepository } from "repositories/CategoriesRepository";
+import { CreateCategoryService } from "service/CreateCategoryService";
+
+export const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository()
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ description, name });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list()
+
+  return response.json(all)
+})
