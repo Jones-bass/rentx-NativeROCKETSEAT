c30e1caf6a3a41ec4fc5bbc03eb08d6d3291e86a
jones-bass <jonesbass.tb@gmail.com>
2024-01-31 06:08:43 -0300

    Autenticated Rota

--- server/src/routes/specifications.routes.ts
+++ server/src/routes/specifications.routes.ts
@@ -1,8 +1,11 @@
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecifications/CreateSpecificationController'
+import { ensureAuthenticated } from '@shared/middlewares/ensureAuthenticated'
 import { Router } from 'express'
 
 export const specificationsRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
+
+specificationsRoutes.use(ensureAuthenticated)
 
 specificationsRoutes.post('/', createSpecificationController.handle)
---
+++ server/src/shared/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+
+import { AppError } from '@shared/errors/AppError'
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository'
+
+interface IPayload {
+  sub: string
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401)
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'e7ff0497d496f5f48201740a0860c8d3',
+    ) as IPayload
+
+    request.user = {
+      id: user_id,
+    }
+
+    const usersRepository = new UsersRepository()
+    const user = await usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('Is not authenticated!')
+    }
+
+    next()
+  } catch {
+    throw new AppError('Invalid token!', 401)
+  }
+}
