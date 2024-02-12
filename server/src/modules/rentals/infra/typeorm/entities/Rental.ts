760d27d5485185b8ea8fe545b6372de18d645fb8
jones-bass <jonesbass.tb@gmail.com>
2024-02-12 16:19:34 -0300

    Iniciando Estrutura de Rentls

---
+++ server/src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { v4 as uuidV4 } from 'uuid'
+
+import { Car } from '@modules/cars/infra/typeorm/entities/Car'
+
+@Entity('rentals')
+export class Rental {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: 'car_id' })
+  car: Car
+
+  @Column({ type: 'text' })
+  car_id: string
+
+  @Column({ type: 'text' })
+  user_id: string
+
+  @Column({ type: 'text' })
+  start_date: Date
+
+  @Column({ type: 'text' })
+  end_date: Date
+
+  @Column({ type: 'text' })
+  expected_return_date: Date
+
+  @Column({ type: 'text' })
+  total: number
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
